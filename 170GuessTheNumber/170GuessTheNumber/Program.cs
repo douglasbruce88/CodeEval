c1ad69d762a57e0730eb1aec0cd39e844d59d74f
douglasbruce88 <douglasbruce88@gmail.com>
2014-11-23 11:41:24 +0000

    CodeEval challenges

---
+++ 170GuessTheNumber/170GuessTheNumber/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace GuessTheNumber
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var games = File.ReadAllLines (args [0]);
+
+			//string[] games = { "100 Lower Lower Higher Lower Lower Lower Yay!", "948 Higher Lower Yay!" };
+
+			foreach (var game in games)
+			{
+				var entries = game.Split (' ');
+
+				int minNumber = 0;
+				int maxNumber;
+
+				Int32.TryParse(entries [0], out maxNumber);
+
+				var guess = Math.Ceiling ((double)maxNumber / 2);
+
+				var incorrectGuess = true;
+
+				var i = 1;
+				while (incorrectGuess)
+				{
+					switch (entries [i]) {
+
+					case "Lower":
+						{
+							maxNumber = Convert.ToInt16(guess) - 1;
+							guess = Math.Ceiling ((double)(guess-1+minNumber) / 2);
+							break;
+						}
+					case "Higher":
+						{
+							minNumber = Convert.ToInt16(guess) + 1;
+							guess = Math.Ceiling ((double)(maxNumber+guess+1) / 2);
+							break;
+						}
+					case "Yay!":
+						{
+							Console.WriteLine (guess);
+							incorrectGuess = false;
+							break;
+						}
+					}
+
+					i++;
+				}
+			}
+		}
+	}
+}
---
+++ 3PrimePalindrome/PrimePalindrome/PrimePalindrome/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace PrimePalindrome
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			// Print out the largest prime palindrome less than 'n'
+			var n = 1000;
+
+			bool found = false;
+
+			while (!found) 
+			{
+				if (IsPrime (n) && IsPalindrome (n)) 
+				{
+					found = true;
+					Console.WriteLine (n);
+				}
+					
+				n--;
+			}
+		}
+
+		public static bool IsPrime (int n)
+		{
+			for (int i = 2; i < Math.Sqrt (n); i++) 
+			{
+				var absVal = (double)n / (double)i;
+				var floorVal = Math.Floor ((double)(n / i));
+				if (absVal == floorVal) 
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public static bool IsPalindrome (int n)
+		{
+			var nStr = n.ToString();
+			var nLength = nStr.Length;
+
+			for (int i = 0; i < Math.Ceiling((double)nLength/2); i++) 
+			{
+				if (nStr[i] != nStr[nLength - i - 1])
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+
+
+}
